95174eaeccb4cfdb6045e28b88a1f168ef01cf32
FlippantPope <jonpmiddleton@gmail.com>
2023-07-17 08:39:26 -0700

    Finished frontend app setup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +26,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Outlet />
     </div>
   );
 }
